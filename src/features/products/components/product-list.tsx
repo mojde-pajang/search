c560055a0e9d1610438af0944e0df6426ec1b25f
Mojdeh Pajang <mojde.pajang@gmail.com>
2026-06-07 12:16:51 +0200

    feat: add Skeleton component and integrate into ProductList for loading state

--- src/features/products/components/product-list.tsx
+++ src/features/products/components/product-list.tsx
@@ -4,9 +4,9 @@ import useProducts from "../hooks/use-products";
 import ProductCard from "./product-card";
 import { useState } from "react";
 import type { Product } from "../types/product";
-import Loading from "#components/app/loading";
-import Error from "#components/app/error";
+import Error from "@/components/app/error";
 import { getProducts } from "../api/products.api";
+import { ListProductSkeleton } from "./product-skeleton";
 
 const ProductList = () => {
   const { isError, isLoading, error, products, errorStatus } = useProducts();
@@ -16,14 +16,13 @@ const ProductList = () => {
    return  product.title.toLocaleLowerCase().includes(searchText)
   })
 
-
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value)
 
   };
 
 
-  if (isLoading) return <Loading />
+  if (isLoading) return <ListProductSkeleton />
 
 
   if (isError) return <Error errorMessage={error} errorStatus={errorStatus} fallback={getProducts} />;
